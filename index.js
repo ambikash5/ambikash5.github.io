37133c3e0157e7aad81d655a7199cf91a797b3b1
unknown <kbikash003@gmail.com>
2023-02-10 18:03:27 +0530

    website

---
+++ index.js
@@ -0,0 +1,25 @@
+const mainMenu = document.querySelector('.main-list');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.open-menu');
+const menu_items = document.querySelectorAll('nav .main-list li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
