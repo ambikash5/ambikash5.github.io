acbe0d15d8a0b40df5377bda8ce2cdbff3bb97ab
unknown <kbikash003@gmail.com>
2023-02-16 11:33:52 +0530

    contact section added

--- index.js
+++ index.js
@@ -23,3 +23,9 @@ function show(){
 function close(){
     mainMenu.style.top = '-100%';
 }
+
+function myalert() {
+    alert("Welcome to GeeksforGeeks.\n " +
+        "It is the best portal for computer" +
+                "science enthusiasts!");
+}
