6ebbb5ca1fabfa40bc61d7861d7f6103cb4a3c9a
unknown <kbikash003@gmail.com>
2023-03-06 20:03:30 +0530

    sub services and links added

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const mainMenu = document.querySelector('.main-list');
 const closeMenu = document.querySelector('.close-menu');
 const openMenu = document.querySelector('.open-menu');
 const menu_items = document.querySelectorAll('nav .main-list li a');
+const body = document.querySelector("body");
 
 const service_menu = document.getElementById("service_menu");
 
 function click_services(){
     service_menu.classList.toggle("mega-menu");
-    
+    if (!service_menu.classList.contains("mega-menu")) {
+        // Disable scroll
+        body.style.overflow = "auto";
+    } else {
+        // Enable scroll
+        body.style.overflow = "hidden";
+    }
 }
-
+function close_services(){
+    if (service_menu.classList.contains("mega-menu")) {
+        service_menu.classList.toggle("mega-menu");
+        body.style.overflow = "auto";
+    } 
+}
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
